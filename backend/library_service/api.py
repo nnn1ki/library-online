699cb277ddeaeffd1c76a91ee8f4b8d7bb4dc796
ElectronApps <klimchikeg@gmail.com>
2024-12-02 10:49:24 +0800

    Fix some issues with catalog

--- backend/library_service/api.py
+++ backend/library_service/api.py
@@ -1,49 +1,44 @@
 from rest_framework.decorators import action
 
 from rest_framework.viewsets import GenericViewSet
-from rest_framework import mixins, viewsets
 from rest_framework.response import Response
 
 from library_service.models import *
 
 from serializers import *
 
-class BookViewset(mixins.ListModelMixin, GenericViewSet):
+class BookViewset(GenericViewSet):
     serializer_class = BookSerializer
 
-    @action(methods=["GET"], detail=False)
-    def get_books_list(self, request, *args, **kwargs):
+    def list(self, request, *args, **kwargs):
 
         #TO-DO
 
         return Response()
     
-    @action(url_path="<str: id>", methods=["GET"], detail=False)
-    def get_book_info(self, request, *args, **kwargs):
+    def retrieve(self, request, *args, **kwargs):
 
         #TO-DO
 
         return Response()
     
-    @action(url_path="announcment", methods=["GET"], detail=False)
-    def get_announcements_list(self, request, *args, **kwargs):
+    @action(url_path="announcement", methods=["GET"], detail=False)
+    def announcements_list(self, request, *args, **kwargs):
 
         #TO-DO
 
         return Response()
     
-class LibraryViewset(mixins.ListModelMixin, GenericViewSet):
+class LibraryViewset(GenericViewSet):
     serializer_class = LibrarySerializer
 
-    action(methods=["GET"], detail=False)
-    def get_libraries_list(self, request, *args, **kwargs):
+    def list(self, request, *args, **kwargs):
          
         #TO-DO
 
         return Response()
     
-    action(url_path="<str: name>", methods=["GET"], detail=False)
-    def get_library_info(self, request, *args, **kwargs):
+    def retrieve(self, request, *args, **kwargs):
 
         #TO-DO
 
--- backend/library_service/serializers.py
+++ backend/library_service/serializers.py
@@ -1,15 +1,9 @@
 from rest_framework import serializers
 
-class LibraryNameSerializer(serializers.Serializer):
-    library_name = serializers.CharField()
-
 class LibrarySerializer(serializers.Serializer):
-    library_name = LibraryNameSerializer()
+    library_name = serializers.CharField()
     description = serializers.CharField()
     address = serializers.CharField()
-
-class BookIdSerializer(serializers.Serializer):
-    book_id = serializers.CharField()
 
 class LinkSerializer(serializers.Serializer):
     url = serializers.URLField()
@@ -20,12 +14,11 @@ class CopiesSerializer(serializers.Serializer):
     amount = serializers.IntegerField()
 
 class BookSerializer(serializers.Serializer):
-    id = BookIdSerializer()
+    id = serializers.CharField()
     name = serializers.CharField()
     author = serializers.CharField()
     book_description = serializers.CharField()
     book_logo = serializers.ImageField()
     links = LinkSerializer(many = True)
-    library = LibrarySerializer(many = True)
     copies = CopiesSerializer(many = True)
     can_be_ordered = serializers.BooleanField()
