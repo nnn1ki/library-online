558e2f30026b7dba692a47a90a8ee4adfe3b2d36
yaro-SLAVE <85624526+yaro-SLAVE@users.noreply.github.com>
2025-01-28 11:12:53 +0800

    Add update and delete methods for order API

--- backend/library_service/views/order.py
+++ backend/library_service/views/order.py
@@ -1,25 +1,63 @@
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import mixins
+from rest_framework.exceptions import APIException
 
 from library_service.serializers.order import *
 
 class OrderViewset(
     mixins.ListModelMixin,
     mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
     GenericViewSet
 ):
     permission_classes = [IsAuthenticated]
     queryset = Order.objects.all()
 
     def get_serializer_class(self):
-        if self.action == "list":
+        if self.action in ["list", "destroy", "update"]:
             return OrderSerializer
         elif self.action in ["create"]:
             return CreateOrderSerializer
 
     def get_queryset(self):
         return super().get_queryset().filter(user=self.request.user)
+    
+    def update(self, request, *args, **kwargs):
+        order_id = kwargs["pk"]
+        order = self.get_queryset().filter(pk=order_id).first()
+
+        if (order is None):
+            raise APIException("Order not found", code=404)
+
+        order_statuses = OrderHistory.objects.filter(order=order).all()
+        
+        #Когда статус new, то в истории статусов заказа будет только одна запись
+        if (order_statuses.count() > 1):
+            raise APIException("Order status is not new", code=400)
+        
+        OrderItem.objects.filter(order=order).all().delete()
+
+        self.create(request)
+    
+    def destroy(self, request, *args, **kwargs):
+        order_id = kwargs["pk"]
+        order = self.get_queryset().filter(pk=order_id).first()
+
+        if (order is None):
+            raise APIException("Order not found", code=404)
+
+        #Список допустимых состояний заказа
+        acceptable_statuses = [OrderHistory.Status.NEW, OrderHistory.Status.PROCESSING, OrderHistory.Status.READY]
+        #Нам интересно только последний статус заказа
+        order_last_status = OrderHistory.objects.filter(order=order).last()        
+
+        if (order_last_status.status not in acceptable_statuses):
+            raise APIException("Order status is done or later")
+        
+        order.delete()
+            
 
 class BorrowedViewset (
     mixins.ListModelMixin,
