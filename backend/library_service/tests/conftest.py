5d3f7c06609f614faf32aafb46e8be61741b9f30
ElectronApps <klimchikeg@gmail.com>
2025-02-09 22:37:04 +0800

    Fix mock fixture

--- backend/library_service/tests/conftest.py
+++ backend/library_service/tests/conftest.py
@@ -3,9 +3,12 @@ import multiprocessing
 
 from library_service.tests.opac_mock import run_server
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="session", autouse=True)
 def run_mock_opac(request: pytest.FixtureRequest):
-    mock_process = multiprocessing.Process(run_server)
+    mock_process = multiprocessing.Process(target=run_server)
+    mock_process.daemon = True
+    mock_process.start()
+
     def finalizer(mock_process=mock_process):
         mock_process.terminate()
     request.addfinalizer(finalizer)
