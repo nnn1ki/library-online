71ff28a69537fe46cc80e0cc93c40eb8c2e7fdd7
ElectronApps <klimchikeg@gmail.com>
2025-03-09 21:58:49 +0800

    Revert "Merge branch 'main' into feature/design-system"
    This reverts commit 0842546310f2c0c6bbb6789071d22abaadd924e1, reversing
    changes made to a517e9fa7e25d7a090f37a8ec33e13445e1375e8.

--- client/src/views/HomePage.vue
+++ client/src/views/HomePage.vue
@@ -2,15 +2,9 @@
   <div class="container">
     <BooksSearch />
 
-<<<<<<< HEAD
     <div class="announces">
       <div v-for="book in announces" :key="book.id" class="announce-book">
         <BookCard :book="book" />
-=======
-    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 mt-4">
-      <div v-for="book in announces" :key="book.id" class="col">
-        <AnnouncementBookCard :book="book" />
->>>>>>> main
       </div>
     </div>
   </div>
@@ -18,13 +12,8 @@
 
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
-<<<<<<< HEAD
 import BooksSearch from "@/layouts/BooksSearch.vue";
 import BookCard from "@/components/BookCard.vue";
-=======
-import SearchFilter from "@/layouts/BooksSearch.vue";
-import AnnouncementBookCard from "@/components/AnnouncementBookCard.vue";
->>>>>>> main
 import type { Book } from "@/api/types";
 import { announcesList } from "@/api/announces";
 
