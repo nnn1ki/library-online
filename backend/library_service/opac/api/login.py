77a6a35f5784321abbeacf75ea86c045c9766d1b
yaro-SLAVE <85624526+yaro-SLAVE@users.noreply.github.com>
2025-04-28 14:54:57 +0800

    add get user info method from irbis

--- backend/library_service/opac/api/login.py
+++ backend/library_service/opac/api/login.py
@@ -9,6 +9,19 @@ class AuthResponse(DataClassJsonMixin):
     accessToken = str
     refreshToken = str
 
+class UserInfo(DataClassJsonMixin):
+    dataclass_json_config = config(undefined=Undefined.EXCLUDE)["dataclasses_json"]
+    ticket: str
+    name: str
+    allowed: bool
+    debtor : bool
+    gone: bool
+    academ: bool
+    everlasting: bool
+    category: str | None = None
+    departemnt: str | None = None
+    mail: str | None = None
+
 async def login_reader(client: ClientSession, username: str, password: str) -> AuthResponse:
     payload = {"username": username, "password": password}
 
@@ -30,9 +43,9 @@ async def login_admin(client: ClientSession, username: str, password: str) -> Au
     r.raise_for_status()
     return AuthResponse.schema().load(await r.json())
 
-# async def get_login_info(client: ClientSession, username: str, password: str) -> AuthResponse:
-#     payload = {"username": username, "password": password}
+async def get_login_info(client: ClientSession, accessToken: str) -> UserInfo:
+    headers={"Authorization": f"Bearer {accessToken}"}
 
-#     r = await client.get(f"{settings.OPAC_HOSTNAME}/api/login/info")
-#     r.raise_for_status()
-#     return AuthResponse.schema().load(await r.json())
+    r = await client.get(f"{settings.OPAC_HOSTNAME}/api/readers/info", headers=headers)
+    r.raise_for_status()
+    return UserInfo.schema().load(await r.json())
